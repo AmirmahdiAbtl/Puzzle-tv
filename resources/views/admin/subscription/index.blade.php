dc021e46b7f5240aedf047decbbbe75d97664e0e
Mohamad Choupan <mohamadkhoee@yahoo.com>
2021-05-26 18:19:51 +0430

    create indexing subscribtions and create edit blade for subscriptions

--- resources/views/admin/subscription/index.blade.php
+++ resources/views/admin/subscription/index.blade.php
@@ -17,17 +17,16 @@
                     <th scope="col">مدت </th>
                     <th scope="col">قیمت </th>
                     <th scope="col">ویرایش</th>
-                    <th scope="col">حذف</th>
+                   
 
                 </tr>
             </thead>
             <tbody class="rounded">
                 @forelse ($subscriptions as $subscription)
                 <tr class="rounded">
-                    <td> {{ $subscription->season }} </td>
-                    <td> {{ $episode->title }} </td>
-                    <td> {{ $episode->video }} </td>
-                    <td> {{ $episode->episode_number }} </td>
+                    <td> {{ $subscription->title }} </td>
+                    <td> {{ $subscriptions->time }} </td>
+                    <td> {{ $subscriptions->price}} </td>
                     <td>
                         <div class=" rounded">
                             <button class="btn btn-transparent-danger font-weight-bold mr-2" type="submit"><a class="rounded" href="{{route('episode.edit',['slug'=>$episode->slug])}}"> ویرایش </a></button>
