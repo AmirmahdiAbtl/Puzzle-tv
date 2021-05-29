47640254e731340c5f55626256077419e2aef0c0
Mohamad Choupan <mohamadkhoee@yahoo.com>
2021-05-29 11:22:59 +0430

    edit course episode

--- resources/views/admin/course/edit.blade.php
+++ resources/views/admin/course/edit.blade.php
@@ -1,122 +1,98 @@
 @extends('layouts.admin')
 @section('content')
 <div class="container-fluid">
-    <div class="card card-custom rounded ">
+    <div class="card card-custom rounded">
         <div class="card-header rounded">
             <div class="card-title rounded">
-                <h3 class="card-label rounded ">
-                    افزودن ویدیو
+                <h3 class="card-label rounded font-weight-bolder">
+                    ویرایش ویدیو
                 </h3>
-                <button class="btn btn-bg-secondary btn-hover-bg-danger rounded-circle" id="add">+</button>
             </div>
 
         </div>
-        <div class=" table_scroll card-body rounded col-12 col-md-12  py-3  col-sm-8" style="overflow-x: scroll;">
-            <table class="table col-10   table-striped rounded border-white bg-white ">
-                <thead class="rounded   ">
-                    <tr class=" ">
-                        <th colspan="1"  scope="col">شناسه</th>
-                        <th colspan="1"  scope="col">عنوان</th>
-                        <th colspan="1"   scope="col">بنر فیلم</th>
-                        <th colspan="1"   scope="col">عکس</th>
-                        <th class=" ml-2" colspan="1"   scope="col">اشتراک</th>
-                        <th colspan="1"   scope="col"> ویرایش </th>
-                        <th colspan="1"   scope="col"> دسته بندی </th>
-                        <th colspan="1"  scope="col"> اضافه کردن </th>
-
-
-                        <th   scope="col">حذف</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody id="forms" class=" rounded bg-white  pl-10 pr-26">
-                    <tr class=" rounded   " id="">
-                        <form action="{{ route('course.update',['course' => $course->id]) }}" class="" method="POST" enctype="multipart/form-data">
-                            @method('put')
-                            @if (count($errors)>0)
-                            <td colspan="1">
-                                <div class="alert">
-                                    <ul>
-                                        @foreach ($errors->all() as $error)
-                                        <li>{{$error}}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            </td>
+        <div class="card-body rounded col-12 col-md-12  py-3 col-sm-8">
+            @if (count($errors) > 0)
+            <div class="alert rounded bg-white">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+            <form action="{{ route('course.update',['course' => $course->id]) }}" class="" method="POST" enctype="multipart/form-data">
+                @method('put')
+                @csrf
+                <div class="form-group row">
+                    <div class="col-md-6 col-12">
+                        <label class="font-size-h6 font-weight-bolder text-dark">عنوان</label>
+                        <input  value="{{ $course->title }} " type="text" name="title" class="form-control"  >
+                    </div>
+                    <div class="col-md-6 col-12">
+                        <label class="font-size-h6 font-weight-bolder text-dark">دسته بندی</label>
+                        <select name="categories[]" class="form-control selectpicker" multiple>
+                            <option value=null selected disabled>دسته بندی دوره را انتخاب کنید.
+                            </option>
+                            {{-- @foreach ($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->title }}</option>
+                            @if (count($category->childrenRecursive) > 0)
+                            @include('partials.category',
+                            ['categories'=>$category->childrenRecursive,
+                            'level'=>1])
                             @endif
-                            @csrf
-                            <td colspan="1">
-                                <div class=" rounded ">
-                                    <input value="{{ $course->title }}" class="form-controller rounded" type="text" placeholder="title" name="title">
-                                </div>
-                            </td>
-                            <td>
-                                <div class=" rounded ">
-                                    <input value="{{ $course->discription }}" class="form-controller rounded" type="text" placeholder="discription" name="discription">
-                                </div>
-                            </td>
-                            <td>
-                                <div class=" rounded col-1 ">
-                                    <input class="form-controller rounded" type="file" placeholder="title" name="banner">
-                                </div>
-                            </td>
-                            <td>
-                                <div class=" rounded ">
-                                    <input class="form-controller rounded" type="file" placeholder="title" name="poster">
-                                </div>
-                            </td>
-                            <td>
-                                <div class=" rounded ">
-                                    <select name="status" id="">
-                                        <option value="0">Free</option>
-                                        <option value="1">premium</option>
-                                    </select>
-                                </div>
-                            </td>
-                            <td>
-                                @foreach ($course->category as $item)
-                                    <li class="addedTag">{{ $item->title }}<span style="cursor: pointer" class="tagRemove" onclick="$(this).parent().remove();">x</span>
-                                        <input type="hidden" value="{{ $item->title }}" name="tags[]">
-                                    </li>
-                                @endforeach
-                                    <li class="tagAdd taglist">
-                                        <input type="text" id="search-field">
-                                    </li>
-                            </td>
-                            <td>
-                                <div class=" rounded col-1 ">
-                                    <button class="btn btn-transparent-danger font-weight-bold mr-2 rounded " type="submit">ویرایش</button>
-                                </div>
-                            </td>
-                        </form>
-                        <form action="{{route('course.add_category',['id' => $course->id])}}" method="POST">
-                            @csrf
-                            <td>
-                                <div class=" rounded ">
-                                    <input class="form-controller rounded-2xl" type="text" placeholder="title" name="category_title">
-                                </div>
-                            </td>
-                            <td>
-                                <div class=" rounded col-2  ">
-                                    <button class="btn btn-transparent-danger font-weight-bold mr-2 rounded " type="submit">اضافه کردن دسته بندی </button>
-                                </div>
-                            </td>
-                        </form>
-                        <form action="{{route('course.destroy',['course' => $course->id])}}" method="POST">
-                            @method('delete')
-                            @csrf
-                            <td>
-                                <div class=" rounded">
-                                    <button class=" btn rounded" id="delete${i}"><img src="https://img.icons8.com/flat-round/50/000000/delete-sign.png" width="30 px" height="30px" /></button>
-                                </div>
-                            </td>
-                        </form>
+                            @endforeach --}}
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-12">
+                        <label class="font-size-h6 font-weight-bolder text-dark">توضیحات ویدیو</label>
+                        <textarea name="discription" class="form-control" value="{{$course->discription}} "
+                            placeholder="توضیحات"></textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-md-6 col-12">
+                        <label class="font-size-h6 font-weight-bolder text-dark">پوستر ویدیو</label>
+                        <div class="custom-file mb-3">
+                            <input type="file" name="poster" value="{{ old('poster') }}" class="custom-file-input"
+                                id="customFile">
+                            <label class="custom-file-label" for="customFile">فایل خود را انتخاب کنید</label>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-12">
+                        <label class="font-size-h6 font-weight-bolder text-dark">بنر ویدیو</label>
+                        <div class="custom-file mb-3">
+                            <input type="file" name="banner" value="{{ old('banner') }}" class="custom-file-input"
+                                id="customFile">
+                            <label class="custom-file-label" for="customFile">فایل خود را انتخاب کنید</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-md-6 col-12">
+                        <label class="font-size-h6 font-weight-bolder text-dark">وضعیت ویدیو</label>
+                        <div class="radio-inline mt-2">
+                            <label class="radio radio-lg">
+                                <input type="radio" name="status" value="1" />
+                                <span></span>
+                                اشتراکی
+                            </label>
+                            <label class="radio radio-lg">
+                                <input type="radio" name="status" value="0" checked="checked" />
+                                <span></span>
+                                رایگان
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-12">
 
-                    </tr>
-                </tbody>
-
-            </table>
-
+                    </div>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg font-weight-bolder mt-3">ویرایش ویدیو</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
