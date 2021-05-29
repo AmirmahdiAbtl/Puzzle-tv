75b2ac55ffe1be748529e43ccb2cf4baab5c942a
Mohamad Choupan <mohamadkhoee@yahoo.com>
2021-05-29 11:45:39 +0430

    edit episode part

--- resources/views/admin/episode/edit.blade.php
+++ resources/views/admin/episode/edit.blade.php
@@ -1,110 +1,83 @@
 @extends('layouts.admin')
 @section('content')
 <div class="container-fluid">
-    <div class="card card-custom rounded ">
+    <div class="card card-custom rounded">
         <div class="card-header rounded">
             <div class="card-title rounded">
-                <h3 class="card-label rounded ">
-                    افزودن ویدیو
+                <h3 class="card-label rounded font-weight-bolder">
+                    ویرایش ویدیو
                 </h3>
-                <button class="btn btn-bg-secondary btn-hover-bg-danger rounded-circle" id="add">+</button>
             </div>
 
         </div>
-        <div class=" table_scroll card-body rounded col-12 col-md-12  py-3  col-sm-8" style="overflow-x: scroll;">
-            <table class="table col-10   table-striped rounded border-white bg-white ">
-                <thead class="rounded   ">
-                    <tr class=" ">
+        <div class="card-body rounded col-12 col-md-12  py-3 col-sm-8">
+            @if (count($errors) > 0)
+            <div class="alert rounded bg-white">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+            <form action="{{ route('episode.update',['slug' => $episode->slug]) }}" class="padding-30" method="POST" enctype="multipart/form-data">
+                @method('put')
+                <div class="form-group row">
+                    <div class="col-md-6 col-12">
+                        <label class="font-size-h6 font-weight-bolder text-dark">عنوان</label>
+                        <input  value=" " type="text" name="title" class="form-control"  >
+                    </div>
+                    <div class="col-md-6 col-12">
+                        <label class="font-size-h6 font-weight-bolder text-dark">فصل ها </label>
+                        <select name="categories[]" class="form-control selectpicker" multiple>
+                            <option value=null selected disabled>فصل دوره را انتخاب کنید.
+                            </option>
+                            {{--اینجا فصل ها میائ----}}
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-12">
+                        <label class="font-size-h6 font-weight-bolder text-dark">لینک </label>
+                        <textarea name="discription" class="form-control" value=" "
+                            placeholder="توضیحات"></textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-md-6 col-12">
+                        <label class="font-size-h6 font-weight-bolder text-dark">فایل فیلم</label>
+                        <div class="custom-file mb-3">
+                            <input type="file" name="poster" value="{{ old('poster') }}" class="custom-file-input"
+                                id="customFile">
+                            <label class="custom-file-label" for="customFile">فایل خود را انتخاب کنید</label>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="form-group row">
+                    <div class="col-md-6 col-12">
+                        <label class="font-size-h6 font-weight-bolder text-dark">وضعیت ویدیو</label>
+                        <div class="radio-inline mt-2">
+                            <label class="radio radio-lg">
+                                <input type="radio" name="status" value="1" />
+                                <span></span>
+                                اشتراکی
+                            </label>
+                            <label class="radio radio-lg">
+                                <input type="radio" name="status" value="0" checked="checked" />
+                                <span></span>
+                                رایگان
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-12">
 
-                        <th colspan="1" scope="col">عنوان</th>
-                        <th colspan="1" scope="col">لینک</th>
-                        <th colspan="1" scope="col">فیلم</th>
-                        <th class=" ml-2" colspan="1" scope="col">فصل ها</th>
-                        <th colspan="1" scope="col"> فصل های جدید </th>
-                        <th colspan="1" scope="col"> وضعیت </th>
-                        <th colspan="1" scope="col"> ویرایش </th>
-
-
-                        <th scope="col">حذف</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody id="forms" class=" rounded bg-white  pl-10 pr-26">
-                    <tr class=" rounded   " id="">
-                        <form action="{{ route('episode.update',['slug' => $episode->slug]) }}" class="padding-30" method="POST" enctype="multipart/form-data">
-                            @method('put')
-                            @if (count($errors)>0)
-                            <div class="alert rounded bg-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                    <li>{{$error}}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                            @endif
-                            @csrf
-                            <td>
-                                <div class="rounded">
-                                    <input value="{{ $episode->title }}" class="form-controller rounded" type="text" placeholder="title" name="title">
-                                </div>
-                            </td>
-                            <td>
-                                <div class="rounded">
-                                    <input value="{{ $episode->slug }}" class="form-controller rounded" type="text" placeholder="discription" name="slug">
-                                </div>
-                            </td>
-                            <td>
-                                <div class="rounded">
-                                    <input class="form-controller rounded" type="file" placeholder="title" name="video">
-                                </div>
-                            </td>
-                            <td>
-                            <div class="rounded">
-                                <select value="{{ $episode->season_id }}" name="season_id" id="seasons-select">
-                                    @foreach ($episode->course->seasons as $item)
-                                    <option value="{{ $item->id }}">{{ $item->title }}</option>
-                                    @endforeach
-                                    <option value="0">فصل جدید</option>
-                                </select>
-                            </div>
-</td>
-<td>
-                            <div class="rounded">
-
-                                <input type="text" value="{{ $episode->course->seasons->count() + 1 }}" name="season_id" class="new-seasons rounded" disabled>
-                                <input type="text" name="season_title" class="new-seasons rounded" disabled>
-                            </div>
-</td>
-<td>
-
-                            <div class="rounded">
-                                <select value="{{ $episode->status }}" name="status" id="">
-                                    <option value="0">Free</option>
-                                    <option value="1">Premioum</option>
-                                </select>
-                            </div>
-</td>
-<td>
-                            <div class="rounded">
-                                <button type="btn btn-transparent-danger font-weight-bold mr-2 rounded">ویرایش</button>
-                            </div>
-</td>
-                        </form>
-                        <form action="{{route('episode.delete',['slug' => $episode->slug])}}" method="POST">
-                            @method('delete')
-                            @csrf
-                            <td>
-                                <div class=" rounded">
-                                    <button class=" btn btn-transparent-danger font-weight-bold mr-2 rounded" id="delete ${i}"><img src="https://img.icons8.com/flat-round/50/000000/delete-sign.png" width="30 px" height="30px" /></button>
-                                </div>
-                            </td>
-                        </form>
-
-                    </tr>
-                </tbody>
-
-            </table>
-
+                    </div>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg font-weight-bolder mt-3">ویرایش قسمت</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
--- resources/views/admin/episode/index.blade.php
+++ resources/views/admin/episode/index.blade.php
@@ -6,23 +6,17 @@
             <span class="card-label font-weight-bolder text-dark">لیست قسمت ها</span>
         </h3>
         <div class="card-toolbar">
-            <a href="{{route('episode.create',['id'=>$course->id])}}"
-                class="btn btn-success font-weight-bolder font-size-sm">
+            <a href="{{route('episode.create',['id'=>$course->id])}}" class="btn btn-success font-weight-bolder font-size-sm">
                 <span class="svg-icon svg-icon-md svg-icon-white">
-                    <!--begin::Svg Icon | path:C:\wamp64\www\keenthemes\themes\metronic\theme\html\demo7\dist/../src/media/svg/icons\Files\Folder-plus.svg--><svg
-                        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px"
-                        height="24px" viewBox="0 0 24 24" version="1.1">
+                    <!--begin::Svg Icon | path:C:\wamp64\www\keenthemes\themes\metronic\theme\html\demo7\dist/../src/media/svg/icons\Files\Folder-plus.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
                         <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                             <rect x="0" y="0" width="24" height="24" />
-                            <path
-                                d="M3.5,21 L20.5,21 C21.3284271,21 22,20.3284271 22,19.5 L22,8.5 C22,7.67157288 21.3284271,7 20.5,7 L10,7 L7.43933983,4.43933983 C7.15803526,4.15803526 6.77650439,4 6.37867966,4 L3.5,4 C2.67157288,4 2,4.67157288 2,5.5 L2,19.5 C2,20.3284271 2.67157288,21 3.5,21 Z"
-                                fill="#000000" opacity="0.3" />
-                            <path
-                                d="M11,13 L11,11 C11,10.4477153 11.4477153,10 12,10 C12.5522847,10 13,10.4477153 13,11 L13,13 L15,13 C15.5522847,13 16,13.4477153 16,14 C16,14.5522847 15.5522847,15 15,15 L13,15 L13,17 C13,17.5522847 12.5522847,18 12,18 C11.4477153,18 11,17.5522847 11,17 L11,15 L9,15 C8.44771525,15 8,14.5522847 8,14 C8,13.4477153 8.44771525,13 9,13 L11,13 Z"
-                                fill="#000000" />
+                            <path d="M3.5,21 L20.5,21 C21.3284271,21 22,20.3284271 22,19.5 L22,8.5 C22,7.67157288 21.3284271,7 20.5,7 L10,7 L7.43933983,4.43933983 C7.15803526,4.15803526 6.77650439,4 6.37867966,4 L3.5,4 C2.67157288,4 2,4.67157288 2,5.5 L2,19.5 C2,20.3284271 2.67157288,21 3.5,21 Z" fill="#000000" opacity="0.3" />
+                            <path d="M11,13 L11,11 C11,10.4477153 11.4477153,10 12,10 C12.5522847,10 13,10.4477153 13,11 L13,13 L15,13 C15.5522847,13 16,13.4477153 16,14 C16,14.5522847 15.5522847,15 15,15 L13,15 L13,17 C13,17.5522847 12.5522847,18 12,18 C11.4477153,18 11,17.5522847 11,17 L11,15 L9,15 C8.44771525,15 8,14.5522847 8,14 C8,13.4477153 8.44771525,13 9,13 L11,13 Z" fill="#000000" />
                         </g>
                     </svg>
-                    <!--end::Svg Icon--></span>
+                    <!--end::Svg Icon-->
+                </span>
                 قسمت جدید
             </a>
         </div>
@@ -46,12 +40,36 @@
                     <td> {{ $episode->video }} </td>
                     <td> {{ $episode->episode_number }} </td>
                     <td>
-                        <div class=" rounded">
-                            <button class="btn btn-transparent-danger font-weight-bold mr-2" type="submit"><a
-                                    class="rounded" href="{{route('episode.edit',['slug'=>$episode->slug])}}"> ویرایش
-                                </a></button>
 
-                        </div>
+                        <a href="{{route('episode.edit',['slug'=>$episode->slug])}}" class="btn btn-warning text-center btn-icon btn-circle mr-1">
+                            <span class="svg-icon svg-icon-md svg-icon-white" data-container="body" data-toggle="tooltip" data-placement="top" title="ویرایش دوره">
+                                <!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-04-19-122603/theme/html/demo1/dist/../src/media/svg/icons/General/Settings-2.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                        <rect x="0" y="0" width="24" height="24" />
+                                        <path d="M5,8.6862915 L5,5 L8.6862915,5 L11.5857864,2.10050506 L14.4852814,5 L19,5 L19,9.51471863 L21.4852814,12 L19,14.4852814 L19,19 L14.4852814,19 L11.5857864,21.8994949 L8.6862915,19 L5,19 L5,15.3137085 L1.6862915,12 L5,8.6862915 Z M12,15 C13.6568542,15 15,13.6568542 15,12 C15,10.3431458 13.6568542,9 12,9 C10.3431458,9 9,10.3431458 9,12 C9,13.6568542 10.3431458,15 12,15 Z" fill="#000000" />
+                                    </g>
+                                </svg>
+                                <!--end::Svg Icon-->
+                            </span>
+                        </a>
+                        <form style="display: inline" action="{{route('episode.delete',['slug' => $episode->slug])}}" method="POST">
+                            @method('delete')
+                            @csrf
+                            <button type="submit" class="btn btn-outline-danger btn-icon mr-1" data-toggle="modal" data-target="">
+                                <span class="svg-icon" data-container="body" data-toggle="tooltip" data-placement="top" title="حذف دوره">
+                                    <!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-04-19-122603/theme/html/demo1/dist/../src/media/svg/icons/Home/Trash.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                            <rect x="0" y="0" width="24" height="24" />
+                                            <path d="M6,8 L18,8 L17.106535,19.6150447 C17.04642,20.3965405 16.3947578,21 15.6109533,21 L8.38904671,21 C7.60524225,21 6.95358004,20.3965405 6.89346498,19.6150447 L6,8 Z M8,10 L8.45438229,14.0894406 L15.5517885,14.0339036 L16,10 L8,10 Z" fill="#000000" fill-rule="nonzero" />
+                                            <path d="M14,4.5 L14,3.5 C14,3.22385763 13.7761424,3 13.5,3 L10.5,3 C10.2238576,3 10,3.22385763 10,3.5 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z" fill="#000000" opacity="0.3" />
+                                        </g>
+                                    </svg>
+                                    <!--end::Svg Icon-->
+                                </span>
+                            </button>
+                        </form>
+
+                                             
                     </td>
 
                 </tr>
