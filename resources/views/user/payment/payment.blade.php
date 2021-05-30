0e8ffb503c8dd29e42da9cef4da999543681431f
Mohamad Choupan <mohamadkhoee@yahoo.com>
2021-05-30 17:40:41 +0430

    user by blade

--- resources/views/user/payment/payment.blade.php
+++ resources/views/user/payment/payment.blade.php
@@ -1,8 +1,42 @@
 @extends('layouts.app')
+
 @section('content')
- 
+<div class="d-flex flex-column flex-root mt-24">
+    <!--begin::Login-->
+    <div class="login login-5 login-signin-on d-flex flex-row-fluid my-10" id="kt_login">
+        <div class="d-flex flex-center bgi-size-cover bgi-no-repeat flex-row-fluid" style="background-image: url(/metronic/theme/html/demo1/dist/assets/media/bg/bg-2.jpg);">
+            <div class="login-form text-center text-white p-7 position-relative overflow-hidden bg-white-o-50 w-400px rounded">
+                <!--begin::Login Header-->
+                <div class="d-flex flex-center mb-3">
+                    <a class="font-weight-boldest display-3 text-white" href="#">ایران&zwnj;تی&zwnj;وی</a>
+                </div>
+                <!--end::Login Header-->
+                <!--begin::Login Sign in form-->
+                <div class="login-signup">
+                    <div class="mb-10">
+                        <h3 class="opacity-40 font-weight-normal">خرید اشتراک </h3>
+                    </div>
+                    @include('partials.alerts')
 
+                    <div class="form-group fv-plugins-icon-container">
+                        <a class="btn btn-outline-white opacity-70 px-15 py-3 mt-5 mb-0 w-100">
+                            <span class="form-control h-auto text-white bg-white-o-5 rounded border-0 py-4 px-8" type="text" placeholder="نام" name="fname" id="name "> اشتراک 1 </span>
+                            <span id="price">قیمت </span>
+                        </a>
+                        <div class="fv-plugins-message-container"></div>
+                    </div>
+                    
+                    <div class="form-group mb-2">
+                        <button id="kt_login_signup_submit" class="btn btn-danger opacity-90 px-15 py-3 w-100">خرید</button>
+                        <a href="{{ route('login') }}" class="btn btn-outline-white opacity-70 px-15 py-3 mt-5 mb-0 w-100">خرید اشتراک</a>
+                    </div>
+                    <div></div>
 
-
-
+                </div>
+                <!--end::Login Sign in form-->
+            </div>
+        </div>
+    </div>
+    <!--end::Login-->
+</div>
 @endsection
