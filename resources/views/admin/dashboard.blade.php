31c995c7ebdfe5338df9b1b2536a70b9cd97faeb
Mohamad Choupan <mohamadkhoee@yahoo.com>
2021-06-02 16:23:32 +0430

    Create dashboard.blade.php

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class=" container-fluid">
+    <div class="row">
+        <a class="navbar-brand font-weight-boldest font-size-h1 text-danger text-hover-danger" href="{{ route('home') }}">ایران&zwnj;تی&zwnj;وی</a>
+    </div>
+    <div>
+        <h5> سامانه مدیریت ویدیو ها</h5>
+    </div>
+</div>
+
+@endsection
+
+@section('js')
+<script src="{{ asset('js/pages/widgets.js') }}"></script>
+@endsection
