eff78c7501830ab4755e35f6680d9ac2791a40ba
Mohamad Choupan <mohamadkhoee@yahoo.com>
2021-06-02 11:24:39 +0430

    s

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,73 +12,60 @@
             </div>
 
         </div>
-        
+
         <div class="card-body form-group row   rounded col-12 col-md-12  py-3 col-sm-8 ">
-            
-             @include('layouts.Dashboard')
-            
+
+            @include('layouts.Dashboard')
+
             <div class=" form-group column col-md-5">
                 <div class="col-12    ">
-                    <div class="card rounded border-1   border-white ">
-                        <h5 class="card-header">لیست تراکنش ها</h5>
-                        <div class="card-body">
-                            <table class="  table table-striped">
-                                <thead >
-                                    <tr>
-                                        <th scope="col">نام </th>
-                                        <th scope="col">شروع </th>
-                                        <th scope="col">پایان </th>
-                                        <th scope="col">قیمت</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($payments as $payment)
-                                    <tr>
-                                        <td>{{$payment->title}}</td>
-                                        <td>{{$payment->start_sub}}</td>
-                                        <td>{{$payment->expire_sub}}</td>
-                                        <td>{{$payment->price}}</td>
-                                    </tr>
-                                    @endforeach
+                    <div class=" offset-2 col-md-4 col-6">
+                        <div class="card rounded border-1   border-primary">
+                            <div class=" col-12   mt-5">
+                                <div class="card rounded border-1   border-white">
+                                    <h5 class="card-header">اشتراک شما</h5>
+                                    <div class="card-body">
+                                        {{-- @if ($user->expire_subscription == Null)
+                            <h5 class="card-title">0روز </h5>
+                        @else    
+                            <h5 class="card-title">{{$user->expire_subscription->diffInDays(now())}}</h5>
+                                        @endif --}}
+                                        <h5 class="card-title">{{$user->expire_subscription}} تاریخ پایان اشتراک</h5>
+                                        <a href="" class="btn btn-primary">خرید اشتراک</a>
+                                        <a href="" class="btn btn-white text-black">خرید اشتراک</a>
+                                    </div>
+                                </div>ّ
+                            </div>
 
-                                    <p>
-                                        هیچ تراکنشی برای نمایش وجود ندارد.
-                                    </p>
-                                </tbody>
-                            </table>
                         </div>
                     </div>
                 </div>
-              
             </div>
-        </div>
-    </div>
-</div>
 
 
 
-@endsection
+            @endsection
 
-@section('js')
-<script src="{{ asset('js/pages/widgets.js') }}"></script>
-@endsection
-@section('css')
-<style>
-    #dashboard>div,
-    h5,
-    table,
-    thead,
-    tbody {
-        background-color: rgb(38, 38, 38) !important;
-        color: white;
-    }
+            @section('js')
+            <script src="{{ asset('js/pages/widgets.js') }}"></script>
+            @endsection
+            @section('css')
+            <style>
+                #dashboard>div,
+                h5,
+                table,
+                thead,
+                tbody {
+                    background-color: rgb(38, 38, 38) !important;
+                    color: white;
+                }
 
-    .card,
-    h5 {
-        background-color: rgb(70, 70, 75) !important;
-        color: white;
-    }
-</style>
+                .card,
+                h5 {
+                    background-color: rgb(70, 70, 75) !important;
+                    color: white;
+                }
+            </style>
 
 
-@endsection
+            @endsection
